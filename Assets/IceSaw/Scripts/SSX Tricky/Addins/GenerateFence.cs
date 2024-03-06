2a4db7a0d3668ba4efbfdc9e5bdd92253c4fcc9b
GlitcherOG <54088472+GlitcherOG@users.noreply.github.com>
2024-03-06 23:59:41 +1100

    Update GenerateFence.cs

--- Assets/IceSaw/Scripts/SSX Tricky/Addins/GenerateFence.cs
+++ Assets/IceSaw/Scripts/SSX Tricky/Addins/GenerateFence.cs
@@ -2,6 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEditor;
 using UnityEngine;
+using static TrickyLevelManager;
 
 [ExecuteInEditMode]
 public class GenerateFence : MonoBehaviour
@@ -11,20 +12,82 @@ public class GenerateFence : MonoBehaviour
     public bool FlipTextureY;
     public int TextureID;
 
+    [ContextMenu("DrawMesh")]
+    public void GenerateMesh()
+    {
+        for (int i = 0; i < Points.Count-1; i++)
+        {
+            Mesh mesh = GenerateFenceSection(i);
+
+            GameObject gameObject = new GameObject(i.ToString());
+            gameObject.transform.parent = transform;
+            gameObject.transform.localRotation = new Quaternion(0, 0, 0, 1);
+            gameObject.transform.localScale = new Vector3(1, 1, 1);
+            gameObject.transform.localPosition = new Vector3(0,0,0);
+
+            var TempMaterial = new Material(Shader.Find("ModelShader"));
+            Material mat = new Material(TempMaterial);
+
+            Texture2D texture = null;
+            if (TextureID<TrickyLevelManager.Instance.texture2Ds.Count)
+            {
+                texture = TrickyLevelManager.Instance.texture2Ds[TextureID].Texture;
+            }
+
+            TempMaterial.SetTexture("_MainTexture", texture);
+            TempMaterial.SetFloat("_OutlineWidth", 0);
+            TempMaterial.SetFloat("_OpacityMaskOutline", 0f);
+            TempMaterial.SetColor("_OutlineColor", new Color32(255, 255, 255, 0));
+            TempMaterial.SetFloat("_NoLightMode", 1);
+
+
+            gameObject.AddComponent<MeshFilter>().sharedMesh = mesh;
+            gameObject.AddComponent<MeshRenderer>().sharedMaterial = TempMaterial;
+
+
+        }
+    }
+
     public Mesh GenerateFenceSection(int ID)
     {
         Mesh mesh = new Mesh();
-        List<Vector3> Points = new List<Vector3>();
+        mesh.name = "Mesh1";
+        List<Vector3> MeshPoints = new List<Vector3>();
 
-        Points.Add(Points[ID + 0]);
-        Points.Add(Points[ID + 1]);
-        //Points.Add();
+        Vector3 Point1 = Points[ID + 0];
+        Vector3 Point2 = Points[ID + 1];
 
-        Points.Add(Points[ID + 1]);
-        //Points.Add();
-        //Points.Add();
+        Vector3 Point3 = Point1 + new Vector3(0,0,ZSize);
+        Vector3 Point4 = Point2 + new Vector3(0, 0, ZSize);
 
-        Points
+        MeshPoints.Add(Point1);
+        MeshPoints.Add(Point2);
+        MeshPoints.Add(Point3);
+        MeshPoints.Add(Point4);
+
+        List<int> Indices = new List<int>();
+
+        Indices.Add(0);
+        Indices.Add(1);
+        Indices.Add(2);
+
+        Indices.Add(3);
+        Indices.Add(2);
+        Indices.Add(1);
+
+
+        List<Vector2> vector2s = new List<Vector2>();
+
+        vector2s.Add(new Vector2(0, 0));
+        vector2s.Add(new Vector2(1, 0));
+        vector2s.Add(new Vector2(0, 1));
+        vector2s.Add(new Vector2(1, 1));
+        //Points
+
+        mesh.vertices = MeshPoints.ToArray();
+        mesh.triangles = Indices.ToArray();
+        mesh.uv = vector2s.ToArray();
+        mesh.Optimize();
 
         return mesh;
     }
@@ -41,17 +104,20 @@ public class GenerateFence : MonoBehaviour
 
     private void OnSceneGUI(SceneView sceneView)
     {
-        for (int i = 0; i < Points.Count-1; i++)
+        if (Points.Count != 0)
         {
-            Handles.DrawLine(TrickyLevelManager.Instance.transform.TransformPoint(Points[i]), TrickyLevelManager.Instance.transform.TransformPoint(Points[i + 1]), 6f);
+            for (int i = 0; i < Points.Count - 1; i++)
+            {
+                Handles.DrawLine(TrickyLevelManager.Instance.transform.TransformPoint(Points[i]), TrickyLevelManager.Instance.transform.TransformPoint(Points[i + 1]), 6f);
 
-            Vector3 Point1 = Points[i];
-            Vector3 Point2 = Points[i + 1];
+                Vector3 Point1 = Points[i];
+                Vector3 Point2 = Points[i + 1];
 
-            Point1.z += ZSize;
-            Point2.z += ZSize;
+                Point1.z += ZSize;
+                Point2.z += ZSize;
 
-            Handles.DrawLine(TrickyLevelManager.Instance.transform.TransformPoint(Point1), TrickyLevelManager.Instance.transform.TransformPoint(Point2), 6f);
+                Handles.DrawLine(TrickyLevelManager.Instance.transform.TransformPoint(Point1), TrickyLevelManager.Instance.transform.TransformPoint(Point2), 6f);
+            }
         }
     }
 }
