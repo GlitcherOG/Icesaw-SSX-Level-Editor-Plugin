fb019bd5701e6d95c62dec855ac248ec4ff61f02
GlitcherOG <54088472+GlitcherOG@users.noreply.github.com>
2024-10-27 18:46:43 +1100

    Update TrickyTransform.cs

--- Assets/IceSaw/Scripts/SSX Tricky/TrickyTransform.cs
+++ Assets/IceSaw/Scripts/SSX Tricky/TrickyTransform.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEditor;
@@ -8,16 +9,28 @@ using UnityEngine.UIElements;
 [ExecuteInEditMode]
 public class TrickyTransform : MonoBehaviour
 {
+    public static Matrix4x4 SSXMatrix;
+    public static Matrix4x4 SSXRotationMatrix;
+    public static Matrix4x4 SSXScaleMatrix;
+
     public Vector3 Position;
     public Vector3 Rotation;
     public Vector3 Scale;
 
     public void Awake()
     {
-        this.transform.hideFlags = HideFlags.HideInInspector;
+        //if(SSXMatrix == null)
+        //{
+        SSXMatrix = Matrix4x4.TRS(new Vector3(0,0,0), Quaternion.Euler(new Vector3(-90, 0, 0)), new Vector3(1, -1, 1) * SSXProjectWindow.Scale);
+        SSXRotationMatrix = Matrix4x4.Rotate(Quaternion.Euler(new Vector3(-90, 0, 0)));
+        SSXScaleMatrix = Matrix4x4.Scale(new Vector3(1, -1, 1) * SSXProjectWindow.Scale);
+        //}
 
-        Position = this.transform.localPosition;
-        Rotation = this.transform.localEulerAngles;
-        Scale = this.transform.localScale;
+        //this.transform.hideFlags = HideFlags.HideInInspector;
+
+        Position = SSXMatrix.inverse.MultiplyPoint(transform.localPosition);
+        Rotation = SSXRotationMatrix.inverse.MultiplyPoint(transform.localEulerAngles);
+        Scale = SSXScaleMatrix.inverse.MultiplyPoint(transform.localScale);
     }
+
 }
