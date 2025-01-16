a623fa10517aad1cc4bb7b110a6d2ed70562b702
GlitcherOG <54088472+GlitcherOG@users.noreply.github.com>
2025-01-17 01:04:07 +1100

    OBJ To Instance

---
+++ Assets/IceSaw/Scripts/SSX Tricky/Tools/ObjInstanceImport.cs
@@ -0,0 +1,18 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ObjInstanceImport : MonoBehaviour
+{
+
+    void LoadOBJGeneratePrefabandInstance()
+    {
+        //Load OBJ Plus MLT
+
+        //Generate Material
+
+        //Generate Prefab
+
+        //Generate Instance
+    }
+}
