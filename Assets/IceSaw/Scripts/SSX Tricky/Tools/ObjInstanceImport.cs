ff58753c2c8458f5847002568ea499592b8e0eb5
GlitcherOG <54088472+GlitcherOG@users.noreply.github.com>
2025-02-24 19:28:54 +1100

    Update ObjInstanceImport.cs

--- Assets/IceSaw/Scripts/SSX Tricky/Tools/ObjInstanceImport.cs
+++ Assets/IceSaw/Scripts/SSX Tricky/Tools/ObjInstanceImport.cs
@@ -1,5 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
+using System.IO;
+using UnityEditor;
 using UnityEngine;
 
 public class ObjInstanceImport : MonoBehaviour
@@ -7,7 +9,13 @@ public class ObjInstanceImport : MonoBehaviour
 
     void LoadOBJGeneratePrefabandInstance()
     {
+        string path = EditorUtility.OpenFilePanel("Import OBJ Model", "", "obj");
         //Load OBJ Plus MLT
+
+        string FileName = Path.GetFileName(path);
+
+        File.Copy(path, TrickyLevelManager.Instance.LoadPath + "\\Models\\" + FileName);
+        //TrickyLevelManager.Instance.ReloadModels();
 
         //Generate Material
 
